88226f426dafb94d8d42893ef5bd39966f6a34fb
itzelserrano <124410725+itzelserrano@users.noreply.github.com>
2023-09-11 19:22:11 -0700

    11-sep-2023

--- Parcial1/pares.js
+++ Parcial1/pares.js
@@ -7,6 +7,23 @@ $(document).ready(function (){
         var nombre_imagen = $(this).attr('data-id')
 
         if(estado=="0"){
+            //Si las cartas son diferentes
+            if(carta1 !="" && carta2 !=""){
+                console.log("Las carta son difentes")
+                if(carta1.attr('data-id') != carta2.attr('data-id')){
+                    console.log("Tapa carta diferentes")
+                    carta1.attr('src', 'imagenes/portada.jpg')
+                    carta2.attr('src', 'imagenes/portada.jpg')
+                }else{
+                    console.log("Las cartas son iguales")
+                    carta1.attr('data-estado', '1')
+                    carta2.attr('data-estado', '1')
+                    par=true
+                }
+                carta1=""
+                carta2=""
+                return;
+            }
             console.log("Estados de la carta: "+ estado)
             if(carta1==""){
                 carta1=$(this);
@@ -19,13 +36,25 @@ $(document).ready(function (){
             }
         }
 
-        if(carta1 !="" && carta2!=""){
+        if(carta1 !="" && carta2 !=""){
             if(carta1.attr('data-id') ==carta2.attr('data-id')){
-                par=truetotal_pares++;
-                $("#total_pares").html(total_pares);
-                carta1="";
-                carta2="";
+                par=true
+                total_pares++
+                $("#total_pares").html(total_pares)
+                carta1=""
+                carta2=""
             }
+        }
+        if(total_pares == 8){
+            $('img').each(function(){
+                $(this).attr('src', 'imagenes/portada.jpg')
+                $(this).attr('data-estado','0')
+            });
+            total_pares=0
+            par=false
+            carta1=""
+            carta2=""
+            return;
         }
 
     });
