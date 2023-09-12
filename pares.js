c00504aa4b211865c1f8721f1a957190686335ea
itzelserrano <124410725+itzelserrano@users.noreply.github.com>
2023-09-11 18:08:46 -0700

    Delete pares.js

--- pares.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var contador =0;
-    $('img').click(function(e){
-        var nombre_imagen =$(this).attr('data-id')
-        $(this).attr('src', 'imagenes/' + nombre_imagen);
-    });
-
-});
