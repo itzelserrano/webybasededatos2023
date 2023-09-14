e21d25035b9ce45457379074e0505fa2c5aa6c2f
itzelserrano <124410725+itzelserrano@users.noreply.github.com>
2023-09-13 19:29:44 -0700

    13-sep-2023

---
+++ Parcial1/practica4.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var valor_dado=0;
+
+    $("#boton").click(function (e) { 
+        valor_dado=Math.floor(Math.random()*6+1)
+        console.log("Sacaste un: " + valor_dado)
+        $("#dado").attr("src","numeros/dado")
+    });
+});
