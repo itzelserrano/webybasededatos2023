755eed8814239d440cc485a1b7f5a5a427151e71
itzelserrano <124410725+itzelserrano@users.noreply.github.com>
2023-10-18 19:11:37 -0700

    18-oct-2023

--- Parcial2/ConsultarAlumnos.php
+++ Parcial2/ConsultarAlumnos.php
@@ -15,6 +15,45 @@
         $datos=$conexion->query($sql);
     ?>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <?php if("$datos->num_row->0")(?>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <td>ID</td>
+                                <td>Nombre</td>
+                                <td>Numero de control</td>
+                                <td>Semestre</td>
+                                <td>Edad</td>
+                                <td>Turno</td>
+                                <td>Sexo</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php while($registro = $dato->fetch_assco()){?>
+                            <tr>
+                                <td> <?php echo $registro["id"];?> </td>
+                                <td> <?php echo $registro["nombre"];?> </td>
+                                <td> <?php echo $registro["numero_control"];?> </td>
+                                <td> <?php echo $registro["semestre"];?> </td>
+                                <td> <?php echo $registro["edad"];?> </td>
+                                <td> <?php echo $registro["turno"];?> </td>
+                                <td> <?php echo $registro["sexo"];?> </td>
+                                <td>
+                                    <a href="" class="btn btn-xs btn-primary"></a>
+                                </td>
+                            </tr>
+                            }
+                        </tbody>
+
+                    </table>
+                    )
+            </div>
+        </div>
+    </div>
+
     <footer>
         CETIs 107 &copy; 2023;
     </footer>
